0b5c7b20d78616a1483199e4e1a39fd743963c72
Eduard Ghazanchyan <eduardgx@gmail.com>
2016-04-30 16:05:10 +0400

    change slider to slick

--- js/main.js
+++ js/main.js
@@ -1,38 +1,14 @@
 $(function() {
-	// resize header on scroll
-	$(window).on('scroll', headerResizeHandler);
-
-	function headerResizeHandler() {
-		var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-			shrinkOn = 0,
-			$header = $('.navbar');
-
-		if (distanceY > shrinkOn) {
-			$header.addClass("smaller");
-		} else {
-			$header.removeClass("smaller");
-		}
-	}
-
-	headerResizeHandler();
-
-	// nav menu animated scroll to element	
-	$('.nav a, .prognosis-library').on('click', function(e) {
-		var id = $(this).attr('href');
-
-		if (id[0] == '#') {
-			e.preventDefault();
-			// close responsive menu if open
-			var $navbarCollapse = $('#navbarCollapse');
-			var navbarOpen = $navbarCollapse.hasClass('in') || $navbarCollapse.hasClass('collapsing');
-			if (navbarOpen) {
-				$navbarCollapse.collapse('hide');
-			}
-
-			// scroll to element
-			$('html, body').animate({
-				scrollTop: $(id).offset().top - 52
-			}, 1000);
-		}
+	$('.carousel-inner').slick({
+		dots: false,
+		infinite: true,
+		speed: 1000,
+		pauseOnHover: false,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		fade: true,
+		cssEase: 'linear',
+		nextArrow: ".left.carousel-control",
+		prevArrow: ".right.carousel-control"
 	});
 });
