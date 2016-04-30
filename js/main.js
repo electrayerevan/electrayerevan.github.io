4a7409cb637e45259cf80f9211c40eef7471415f
Eduard Ghazanchyan <eduardgx@gmail.com>
2016-04-30 16:35:42 +0400

    change images

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ $(function() {
 	$('.carousel-inner').slick({
 		dots: false,
 		infinite: true,
-		speed: 1000,
+		speed: 800,
 		pauseOnHover: false,
 		autoplay: true,
 		autoplaySpeed: 3000,
 		fade: true,
 		cssEase: 'linear',
-		nextArrow: ".left.carousel-control",
-		prevArrow: ".right.carousel-control"
+		prevArrow: ".left.carousel-control",
+		nextArrow: ".right.carousel-control"
 	});
 });
