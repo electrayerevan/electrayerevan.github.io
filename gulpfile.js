3e08929bd164e9d49df4bf927b15446ba4dd6867
Eduard <eduardgx@gmail.com>
2018-10-29 16:53:45 +0400

    optimize size, convert to jpeg

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageResize = require('gulp-image-resize');
 
 gulp.task('mozjpeg', () =>
 	gulp.src('img/**/*.jpg')
@@ -9,3 +10,49 @@ gulp.task('mozjpeg', () =>
 		})]))
 		.pipe(gulp.dest('dist'))
 );
+
+gulp.task('cover', function () {
+	gulp.src('img/products/*.jpg')
+		.pipe(imageResize({
+			width: 1000,
+			height: 1000,
+			upscale: false,
+			cover: true,
+			noProfile: true,
+			imageMagick: true
+		}))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('coverWidth', function () {
+	gulp.src('dist/*.jpg')
+		.pipe(imageResize({
+			width: 1000,
+			upscale: false,
+			cover: true,
+			noProfile: true,
+			imageMagick: true
+		}))
+		.pipe(gulp.dest('dist2'));
+});
+
+gulp.task('resize', function () {
+	gulp.src('img/products/*.png')
+		.pipe(imageResize({
+			height: 1000,
+			upscale: false,
+			cover: true,
+			imageMagick: true,
+			noProfile: true,
+			format: 'jpeg'
+		}))
+		.pipe(imageResize({
+			width: 1000,
+			upscale: false,
+			cover: true,
+			imageMagick: true
+		}))
+		.pipe(gulp.dest('dist2'));
+});
+
+
